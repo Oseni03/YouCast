0fb22565fe8f6bb25cec0badf0427dfdccaa0fe9
Oseni03 <oseniayomide57@gmail.com>
2026-03-01 02:14:32 +0100

    Project restructuring

--- components/wrapper/auth-wrapper.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { SessionProvider } from "next-auth/react";
-
-function AuthWrapper({ children }) {
-	return <SessionProvider>{children}</SessionProvider>;
-}
-
-export default AuthWrapper;
