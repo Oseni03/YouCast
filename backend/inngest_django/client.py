5f50ca4ec41e0f873730d592686b3e247616ec40
Oseni03 <oseniayomide57@gmail.com>
2026-06-03 18:06:59 +0100

    fix: Update WebSub unsubscribe response handling to include status code 200 feat: Add signing key to Inngest client initialization

--- backend/inngest_django/client.py
+++ backend/inngest_django/client.py
@@ -1,10 +1,11 @@
 import logging
 import inngest
 
-from backend.config import settings
+from config import settings
 
 inngest_client = inngest.Inngest(
     app_id="opticast-backend",
     logger=logging.getLogger("inngest"),
     event_key=settings.INNGEST_EVENT_KEY,
+    signing_key=settings.INNGEST_SIGNING_KEY,
 )
